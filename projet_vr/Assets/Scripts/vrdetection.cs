3514ff7d4ab13cdfde6c8fa3efa10756229421c1
2142692 <2142692@cmontmorency.qc.ca>
2023-10-31 11:35:51 -0400

    commencement

---
+++ projet_vr/Assets/Scripts/vrdetection.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Management;
+
+public class vrdetection : MonoBehaviour
+{
+    public bool activVR = true;
+public GameObject sourisClavier;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(activVR == true){
+       var xrParametres = XRGeneralSettings.Instance;
+       if(xrParametres == null) {
+        return;
+       }
+var xrManager = xrParametres.Manager;
+if(xrManager == null){
+    return;
+}
+var xrLoader = xrManager.activeLoader;
+if(xrLoader == null){
+    sourisClavier.SetActive(true);
+    return;
+}
+sourisClavier.SetActive(false);
+        }
+        else{
+            sourisClavier.SetActive(true);
+        }
+
+
+
+    }
+
+    
+}
