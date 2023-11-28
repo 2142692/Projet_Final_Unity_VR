3d33376e2e56c8ff95271791770515f642d9e5c7
2142692 <2142692@cmontmorency.qc.ca>
2023-11-28 14:16:16 -0500

    code_stage2

---
+++ projet_vr/Assets/Scripts/porte_code.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+public class porte_code : MonoBehaviour
+{
+    public Text Ans;
+    public Animator armoire;
+    
+    private string Answer = "1750";
+
+    public void Number(int number)
+    {
+        Ans.text += number.ToString();
+    }
+
+public void Execute()
+    {
+        if(Ans.text == Answer)
+        {
+            Ans.text = "correct";
+            armoire.Play("armoire");
+            
+            
+        }
+
+        else
+        {
+            Ans.text = "";
+           
+        }
+
+    }
+
+}
